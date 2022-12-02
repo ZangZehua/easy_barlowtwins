612545355bf4a8bee5ba197784151f05d29b9712
ZangZehua <zhzang97@gmail.com>
2022-12-02 23:47:53 +0800

    fix

--- evaluate.py
+++ evaluate.py
@@ -111,7 +111,7 @@ def main_worker(gpu, args):
     torch.backends.cudnn.benchmark = True
 
     model = models.resnet50().cuda(gpu)
-    state_dict = torch.load(args.pretrained, map_location='cpu')
+    state_dict = torch.load(args.pre, map_location='cpu')
     missing_keys, unexpected_keys = model.load_state_dict(state_dict, strict=False)
     assert missing_keys == ['fc.weight', 'fc.bias'] and unexpected_keys == []
     model.fc.weight.data.normal_(mean=0.0, std=0.01)
